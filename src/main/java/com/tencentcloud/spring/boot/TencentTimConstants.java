fa2555f4f58db132acfc5e331c0856247e71b318
wandl <hiwepy@163.com>
2020-11-16 13:53:18 +0800

    修改

--- src/main/java/com/tencentcloud/spring/boot/TencentTimConstants.java
+++ src/main/java/com/tencentcloud/spring/boot/TencentTimConstants.java
@@ -11,6 +11,21 @@ public class TencentTimConstants {
 		
 	}
 	
+	/**
+	   * 返回的用户状态，目前支持的状态有：
+	   *前台运行状态（Online）：客户端登录后和即时通信 IM 后台有长连接
+	   * 后台运行状态（PushOnline）：iOS 和 Android 进程被 kill 或因网络问题掉线，进入 PushOnline 状态，此时仍然可以接收消息的离线推送。客户端切到后台，但是进程未被手机操作系统 kill 掉时，此时状态仍是 Online
+	   *未登录状态（Offline）：客户端主动退出登录或者客户端自上一次登录起7天之内未登录过
+	   *如果用户是多终端登录，则只要有一个终端的状态是 Online ，该字段值就是 Online
+	   */
+	public static class State {
+
+		public static final String ONLINE = "Online"; // 前台运行状态（Online）：客户端登录后和即时通信 IM 后台有长连接
+		public static final String PUSHONLINE = "PushOnline"; // 后台运行状态（PushOnline）：iOS 和 Android 进程被 kill 或因网络问题掉线，进入 PushOnline 状态，此时仍然可以接收消息的离线推送。客户端切到后台，但是进程未被手机操作系统 kill 掉时，此时状态仍是 Online
+		public static final String OFFLINE = "Offline"; // 未登录状态（Offline）：客户端主动退出登录或者客户端自上一次登录起7天之内未登录过
+		
+	}
+	
 	public static class CallBack {
 		
 		/**
