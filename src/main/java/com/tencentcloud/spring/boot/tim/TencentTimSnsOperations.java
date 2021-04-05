7ce4b1f795dbe304382cf597d16b45314f12a5df
wandl <hnxxyhcwdl1003@163.com>
2021-04-05 22:26:29 +0800

    优化

--- src/main/java/com/tencentcloud/spring/boot/tim/TencentTimSnsOperations.java
+++ src/main/java/com/tencentcloud/spring/boot/tim/TencentTimSnsOperations.java
@@ -1,21 +1,69 @@
 package com.tencentcloud.spring.boot.tim;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.springframework.util.StringUtils;
+
+import com.google.common.collect.ImmutableMap;
 import com.tencentcloud.spring.boot.tim.req.message.BlacklistMessage;
+import com.tencentcloud.spring.boot.tim.req.sns.FriendItem;
+import com.tencentcloud.spring.boot.tim.resp.AccountCheckResponse;
+import com.tencentcloud.spring.boot.tim.resp.AddFriendResponse;
 import com.tencentcloud.spring.boot.tim.resp.BlacklistResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Tim 资料管理接口集成
- * https://cloud.tencent.com/document/product/269/42440
+ * 关系链管理
+ * https://cloud.tencent.com/document/product/269/1519
  */
 @Slf4j
 public class TencentTimSnsOperations extends TencentTimOperations {
 
 	public TencentTimSnsOperations(TencentTimTemplate timTemplate) {
 		super(timTemplate);
+	}
+	
+	/**
+	 * 1、添加好友
+	 * API：https://cloud.tencent.com/document/product/269/1643
+	 * @param userId 业务用户ID
+	 * @param addType  加好友方式（默认双向加好友方式）：Add_Type_Single 表示单向加好友, Add_Type_Both 表示双向加好友
+	 * @param forceAdd 是否强制相互添加好友
+	 * @param friends 添加的好友数组
+	 * @return 操作结果
+	 */
+	public AddFriendResponse addFriend(String userId, String addType, boolean forceAdd, FriendItem ... friends) {
+		Map<String, Object> requestBody = new ImmutableMap.Builder<String, Object>()
+				.put("From_Account", this.getImUserByUserId(userId))
+				.put("AddFriendItem", Stream.of(friends).map(friend -> {
+					Map<String, Object> userMap = new HashMap<>();
+					userMap.put("To_Account", this.getImUserByUserId(friend.getAccount()));
+					if(StringUtils.hasText(friend.getRemark())) {
+						userMap.put("Remark", friend.getRemark());
+					}
+					if(StringUtils.hasText(friend.getGroupName())) {
+						userMap.put("GroupName", friend.getGroupName());
+					}
+					userMap.put("AddSource", this.getImUserByUserId(friend.getAccount()));
+					if(StringUtils.hasText(friend.getWording())) {
+						userMap.put("AddWording", friend.getWording());
+					}
+					return userMap;
+				}).collect(Collectors.toList()))
+				.put("AddType", addType)
+				.put("ForceAddFlags", forceAdd ? 1 : 0)
+				.build();
+		AddFriendResponse res = request(TimApiAddress.FRIEND_ADD.getValue() + joiner.join(getDefaultParams()),
+				requestBody, AddFriendResponse.class);
+		if (!res.isSuccess()) {
+			log.error("添加好友失败， ActionStatus : {}, ErrorCode : {}, ErrorInfo : {}", res.getActionStatus(), res.getErrorCode(), res.getErrorInfo());
+		}
+		return res;
 	}
 	
 	public Boolean addBlackList(String fromAccount, String toAccount) {
---
+++ src/main/java/com/tencentcloud/spring/boot/tim/resp/AddFriendResultItem.java
@@ -0,0 +1,27 @@
+package com.tencentcloud.spring.boot.tim.resp;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class AddFriendResultItem {
+	
+	/**
+	 * 请求添加的好友的 UserID
+	 */
+	@JsonProperty("To_Account")
+	private String account;
+	/**
+	 * To_Account 的处理结果，0表示成功，非0表示失败，非0取值的详细描述请参见 错误码说明
+	 */
+	@JsonProperty("ResultCode")
+	private Integer resultCode;
+	/**
+	 * To_Account 的错误描述信息，成功时该字段为空
+	 */
+	@JsonProperty("ResultInfo")
+	private String resultInfo;
+	
+}
