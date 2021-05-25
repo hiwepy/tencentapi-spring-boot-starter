f91c0e82fb8891834544d47a80eff7ca8e5c4b42
wandl <hiwepy@163.com>
2021-05-25 09:43:52 +0800

    优化逻辑

--- src/main/java/com/tencentcloud/spring/boot/tim/TencentTimTemplate.java
+++ src/main/java/com/tencentcloud/spring/boot/tim/TencentTimTemplate.java
@@ -196,21 +196,17 @@ public class TencentTimTemplate {
 	            
 	            @Override
 	            public void onResponse(Call call, Response response) {
-	                try {
-	                	if (response.isSuccessful()) {
-	        				try {
-	        					String body = response.body().string();
-	        					log.debug("Async Request Success: code : {}, body : {} , use time : {} ", response.code(), body , System.currentTimeMillis() - start);
-	        					consumer.accept(response);
-	        				} catch (IOException e) {
-	        					log.error(e.getMessage());
-	        				}
-	                    } else {
-	                    	log.error("Async Request Failure : code : {}, message : {}, use time : {} ", response.code(), response.message(), System.currentTimeMillis() - start);
-	        			}
-					} finally {
-						response.close();
-					}
+                	if (response.isSuccessful()) {
+        				try {
+        					String body = response.body().string();
+        					log.debug("Async Request Success: code : {}, body : {} , use time : {} ", response.code(), body , System.currentTimeMillis() - start);
+        					consumer.accept(response);
+        				} catch (IOException e) {
+        					log.error(e.getMessage());
+        				}
+                    } else {
+                    	log.error("Async Request Failure : code : {}, message : {}, use time : {} ", response.code(), response.message(), System.currentTimeMillis() - start);
+        			}
 	            }
 	            
 	        });
