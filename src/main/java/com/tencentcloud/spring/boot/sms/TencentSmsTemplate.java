7102fe3b860cfa3d591f3cf1dcd4ea93bad597c2
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:56:36 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.5.x branch

--- src/main/java/com/tencentcloud/spring/boot/sms/TencentSmsTemplate.java
+++ src/main/java/com/tencentcloud/spring/boot/sms/TencentSmsTemplate.java
@@ -23,88 +23,94 @@ import com.tencentcloudapi.sms.v20190711.SmsClient;
 import com.tencentcloudapi.sms.v20190711.models.SendSmsRequest;
 import com.tencentcloudapi.sms.v20190711.models.SendSmsResponse;
 
+/**
+ * Facade around the Tencent Cloud SMS SDK {@link SmsClient} that simplifies
+ * sending text messages using the bound {@link TencentSmsProperties}
+ * (signature, SdkAppid, sender id, etc.).
+ *
+ * @author [@Loong Wan](https://github.com/loong10k)
+ * @since 1.0.0
+ */
 public class TencentSmsTemplate {
 
-	private SmsClient smsClient;
-	private TencentSmsProperties smsProperties;
-	
+	private final SmsClient smsClient;
+	private final TencentSmsProperties smsProperties;
+
+	/**
+	 * Wraps the given SDK client and configuration.
+	 *
+	 * @param smsClient      the Tencent SMS SDK client
+	 * @param smsProperties  the bound SMS configuration
+	 */
 	public TencentSmsTemplate(SmsClient smsClient, TencentSmsProperties smsProperties) {
 		this.smsClient = smsClient;
 		this.smsProperties = smsProperties;
 	}
 
+	/**
+	 * Sends an SMS to a single phone number using the given approved template.
+	 *
+	 * @param phoneNumber    destination phone number in E.164 format (e.g. {@code +8613711112222})
+	 * @param templateID     approved template id from the SMS console
+	 * @param templateParams template parameter values, in declared order (may be empty)
+	 * @return the SDK response carrying the send status
+	 * @throws TencentCloudSDKException if the SDK call fails
+	 */
 	public SendSmsResponse send(String phoneNumber, String templateID, String... templateParams) throws TencentCloudSDKException {
 		return this.send(new String[] {phoneNumber}, templateID, templateParams);
 	}
-	
+
+	/**
+	 * Sends an SMS to up to 200 phone numbers using the given approved template.
+	 *
+	 * @param phoneNumbers   destination phone numbers in E.164 format (max 200)
+	 * @param templateID     approved template id from the SMS console
+	 * @param templateParams template parameter values, in declared order (may be empty)
+	 * @return the SDK response carrying the send status
+	 * @throws TencentCloudSDKException if the SDK call fails
+	 */
 	public SendSmsResponse send(String[] phoneNumbers, String templateID, String... templateParams) throws TencentCloudSDKException {
-		
-		/*
-		 * 实例化一个请求对象，根据调用的接口和实际情况，可以进一步设置请求参数 您可以直接查询 SDK 源码确定接口有哪些属性可以设置
-		 * 属性可能是基本类型，也可能引用了另一个数据结构 推荐使用 IDE 进行开发，可以方便地跳转查阅各个接口和数据结构的文档说明
-		 */
+
 		SendSmsRequest req = new SendSmsRequest();
-
-		/* 模板 ID: 必须填写已审核通过的模板 ID，可登录 [短信控制台] 查看模板 ID */
 		req.setTemplateID(templateID);
-
-		/*
-		 * 下发手机号码，采用 e.164 标准，+[国家或地区码][手机号] 例如+8613711112222， 其中前面有一个+号
-		 * ，86为国家码，13711112222为手机号，最多不要超过200个手机号
-		 */
 		req.setPhoneNumberSet(phoneNumbers);
-
-		/* 模板参数: 若无模板参数，则设置为空 */
 		req.setTemplateParamSet(templateParams);
 
 		return this.send(req);
 	}
-	
-	/*
-	 * 填充请求参数，这里 request 对象的成员变量即对应接口的入参 您可以通过官网接口文档或跳转到 request 对象的定义处查看请求参数的定义
-	 * 基本类型的设置: 帮助链接： 短信控制台：https://console.cloud.tencent.com/smsv2 sms
-	 * helper：https://cloud.tencent.com/document/product/382/3773
+
+	/**
+	 * Sends an SMS using a fully-customised request, filling the SdkAppid,
+	 * signature, sender id, session context and extension code from the bound
+	 * configuration when those values are present.
+	 *
+	 * @param req the SDK request to enrich and send
+	 * @return the SDK response carrying the send status
+	 * @throws TencentCloudSDKException if the SDK call fails
 	 */
 	public SendSmsResponse send(SendSmsRequest req) throws TencentCloudSDKException {
-		
-		// 批量设置参数
+
+		// Apply all non-null bound properties onto the request.
 		PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
-		
-		/* 1、短信应用 ID: 在 [短信控制台] 添加应用后生成的实际 SDKAppID，例如1400006666 */
+
 		map.from(smsProperties.getSdkappid()).to(req::setSmsSdkAppid);
-
-		/* 2、短信签名内容: 使用 UTF-8 编码，必须填写已审核通过的签名，可登录 [短信控制台] 查看签名信息 */
 		map.from(smsProperties.getSign()).to(req::setSign);
-		
-		/* 3、国际/港澳台短信 senderid: 国内短信填空，默认未开通，如需开通请联系 [sms helper] */
 		map.from(smsProperties.getSenderId()).to(req::setSenderId);
-
-		/* 4、用户的 session 内容: 可以携带用户侧 ID 等上下文信息，server 会原样返回 */
 		map.from(smsProperties.getSession()).to(req::setSessionContext);
-		
-		/* 5、短信码号扩展号: 默认未开通，如需开通请联系 [sms helper] */
 		map.from(smsProperties.getEncode()).to(req::setExtendCode);
-		
-		/*
-		 * 通过 client 对象调用 SendSms 方法发起请求。注意请求方法名与请求对象是对应的 返回的 res 是一个 SendSmsResponse
-		 * 类的实例，与请求对象对应
-		 */
+
 		return smsClient.SendSms(req);
 	}
-	
+
 	/**
-	 * TODO
-	 * @author 		： <a href="https://github.com/vindell">vindell</a>
-	 * @return		: the smsClient
+	 * @return the underlying Tencent SMS SDK client.
 	 */
 	public SmsClient getSmsClient() {
 		return smsClient;
 	}
-	
+
 	/**
-	 * TODO
-	 * @author 		： <a href="https://github.com/vindell">vindell</a>
-	 * @return		: the smsProperties
+	 * @return the bound SMS configuration.
 	 */
 	public TencentSmsProperties getSmsProperties() {
 		return smsProperties;
